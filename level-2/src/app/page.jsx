27ca1d838ac75ae76653ef8186c20e9875b71d79
BHATKAR SAMYAK ANIL <samyakbhatkar9@gmail.com>
2026-07-23 15:11:46 +0000

    Started Dynamic Rounting Learning

---
+++ level-2/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className=" text-8xl flex justify-center items-center h-screen ">
+  <p className="bg-red-400 rounded-full px-4 py-2"> Level 2 Home</p>
+   </div>
+  );
+}
---
+++ level-2/src/app/profile/[username]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full text-center text-4xl font-bold mt-10 '>Dynamic Profile Page.</div>
+  )
+}
+
+export default page
