d84a2ec012bb37a455ee047c5cf3c59ca7440194
BHATKAR SAMYAK ANIL <samyakbhatkar9@gmail.com>
2026-07-24 08:15:17 +0000

    Completed the Learning of Route Segment

--- level-2/src/app/profile/[username]/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div className='w-full text-center text-4xl font-bold mt-10 '>Dynamic Profile Page.</div>
-  )
-}
-
-export default page
--- level-2/src/app/profile/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>This is a profile page.</div>
-  )
-}
-
-export default page
