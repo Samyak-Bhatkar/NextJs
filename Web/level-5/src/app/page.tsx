4cac64dc1e7a52c45cdbd911570ffa77bdbed2e4
BHATKAR SAMYAK ANIL <samyakbhatkar9@gmail.com>
2026-08-08 03:35:44 +0000

    Level-5 Initiate

---
+++ Web/level-5/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen justify-center items-center text-4xl">
+
+
+      SIH Hackathon Winner Solution.....
+    </div>
+    
+  );
+}
