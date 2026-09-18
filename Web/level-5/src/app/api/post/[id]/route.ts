d9e1ae47fe1d10d0c1fdb002a0c172136eed8596
BHATKAR SAMYAK ANIL <samyakbhatkar9@gmail.com>
2026-09-18 05:34:57 +0000

    final update for this codespace as storage is getting full

---
+++ Web/level-5/src/app/api/post/[id]/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+interface paramsType{
+params :{
+    id:number;
+} 
+}
+
+
+
+
+export async function GET(request:NextRequest,{params}:{params:paramsType}){
+
+
+
+
+}
